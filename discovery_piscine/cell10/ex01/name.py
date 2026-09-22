15a2de3be0dcf6155ac53c4fc6e0acee169c9f1b
Hao Zheng <hazheng2@z2t14c2.42bangkok.com>
2026-09-22 15:38:09 +0700

    text003

---
+++ discovery_piscine/cell10/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Hao"
+last_name = "Zheng"
+whole_name = str(first_name+ " " +last_name)
+
+print(whole_name)
---
+++ discovery_piscine/cell10/ex03/whatsyourname.py
@@ -0,0 +1,4 @@
+yourname = str(input("Hey, what's your first name? : "))
+lastname = str(input("And your last name? : "))
+
+print(f"Well, pleased to meet you, {yourname} {lastname}")
